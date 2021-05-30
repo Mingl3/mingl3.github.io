9fda6c3daf2d469c454555688f2ca5c472fedc3d
Jesse Tuglu <jktuglu@gmail.com>
2021-05-30 17:04:48 -0400

    v1.0

--- static/js/main.js
+++ static/js/main.js
@@ -1,19 +1,61 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
+$(document).ready(function () {
+    hideMobile();
+    faq();
 
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-        /* Toggle between adding and removing the "active" class,
-        to highlight the button that controls the panel */
-        this.classList.toggle("active");
 
-        /* Toggle between hiding and showing the active panel */
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "block";
-        }
-        console.log("Working.");
+    $("button").click(function() {
+        $(document).scrollTop($(document).height());
     });
+});
+
+
+// Navbar
+function hideMobile() {
+    var mobile = (/iphone|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));
+    if (mobile) {
+        swapCols();
+        return;
+    }
+    $("#mobile-nav").hide();
 }
+
+// Navbar End
+
+
+// Index
+
+function swapCols() {
+    $("#col-1").removeClass("order-1").addClass("order-2");
+    $("#col-2").removeClass("order-2").addClass("order-1");
+}
+
+// Index End
+
+
+// Contact
+
+
+// Contact End
+
+// FAQ
+function faq() {
+    var faqs = document.getElementsByClassName("faq");
+
+    for (var i = 0; i < faqs.length; i++) {
+        faqs[i].addEventListener("click", function () {
+            this.classList.toggle("active-faq");
+            var panel = this.nextElementSibling;
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+            } else {
+                panel.style.display = "block";
+            }
+        });
+    }
+}
+
+// FAQ End
+
+
+
+
